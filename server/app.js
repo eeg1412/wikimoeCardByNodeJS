78e85d36e34def0ca25a93f0973eceef42d0a222
eeg1412 <eeg1412@163.com>
2019-03-18 17:53:38 +0800

    后端依赖修改

--- server/app.js
+++ server/app.js
@@ -5,15 +5,14 @@ var logger = require('morgan');
 var history = require('connect-history-api-fallback');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/api', usersRouter);
+app.use('/api', apiRouter);
 app.use(history());
 app.use('/',express.static(path.join(__dirname, 'public')));
-
 module.exports = app;
---
+++ server/config/default.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+	port: 3000,
+	url: 'mongodb://localhost:27017/wikimoecard'
+}
