4ed7dc924076bbfac4d6e217189cf4ffee8d6350
eeg1412 <eeg1412@gmail.com>
2020-06-28 16:49:35 +0900

    1.取消了卡牌升级的成功率。 2.卡牌会在20级后，升级需要的卡牌和宝石会逐渐增加。

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 var config = require('config-lite')(__dirname);
 global.myAppConfig = config;//全局设置变量
 global.demining = {
-    onlinePlayer:[],
-    loaing:true
-};//全局矿场信息
+    onlinePlayer: [],
+    loaing: true
+};
+//全局矿场信息
+global.MAX_INT32 = 2147483647;
+global.MAX_INTJS = 9007199254740991;
 var db = require('./mongodb/db');
 var express = require('express');
 var path = require('path');
@@ -16,37 +19,35 @@ const accessLogger = require("./lib/log/accessLogger");  // ADD
 const logger = require("./lib/log/logger").application;
 var compression = require('compression')
 var app = express();
-console.info = (function(oriLogFunc){
-    return function(str)
-    {
-        logger.info('app',str.replace(/\\[[0-9][0-9]m/g, ""));
-        oriLogFunc.call(console,str);
+console.info = (function (oriLogFunc) {
+    return function (str) {
+        logger.info('app', str.replace(/\\[[0-9][0-9]m/g, ""));
+        oriLogFunc.call(console, str);
     }
 })(console.info);
-console.error = (function(oriLogFunc){
-    return function(str)
-    {
-        logger.error('app',str.replace(/\\[[0-9][0-9]m/g, ""));
-        oriLogFunc.call(console,str);
+console.error = (function (oriLogFunc) {
+    return function (str) {
+        logger.error('app', str.replace(/\\[[0-9][0-9]m/g, ""));
+        oriLogFunc.call(console, str);
     }
 })(console.error);
-app.use(compression({filter: shouldCompress}))
+app.use(compression({ filter: shouldCompress }))
 function shouldCompress (req, res) {
- if (req.headers['x-no-compression']) {
-  // 这里就过滤掉了请求头包含'x-no-compression'
-  return false
- }
- 
- return compression.filter(req, res)
+    if (req.headers['x-no-compression']) {
+        // 这里就过滤掉了请求头包含'x-no-compression'
+        return false
+    }
+
+    return compression.filter(req, res)
 }
 app.use(mlogger('dev'));
-app.use(express.json({limit: '1mb'}));
-app.use(express.urlencoded({ extended: false,limit: '1mb' }));
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: false, limit: '1mb' }));
 app.use(cookieParser());
 app.use(session({
     secret: global.myAppConfig.sessionSecret,
     name: 'wikimoe',
-    cookie: {maxAge: 60000},
+    cookie: { maxAge: 60000 },
     resave: false,
     saveUninitialized: true,
 }));
@@ -54,5 +55,5 @@ app.use(session({
 app.use(accessLogger());
 app.use('/api', apiRouter)
 app.use(history());
-app.use('/',express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 module.exports = app;
