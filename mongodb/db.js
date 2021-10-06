0029fb1f10311fde8d2727bd3749594611e97449
Jiang Jiang <eeg1412@gmail.com>
2021-10-06 18:34:10 +0900

    数据库重连逻辑

--- mongodb/db.js
+++ mongodb/db.js
@@ -1,28 +1,29 @@
-var mongoose = require('mongoose');
-var chalk = require('chalk');
-mongoose.connect(process.env.DB_HOST || 'mongodb://localhost:27017/mevntest', { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true });
-mongoose.Promise = global.Promise;
+var mongoose = require('mongoose')
+var chalk = require('chalk')
+mongoose.connect(process.env.DB_HOST || 'mongodb://localhost:27017/mevntest', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+})
+mongoose.Promise = global.Promise
 
-var db = mongoose.connection;
+var db = mongoose.connection
 
 db.once('open', () => {
-    console.info(
-        chalk.green('连接数据库成功')
-    );
+  console.info(chalk.green('连接数据库成功'))
 })
 
 db.on('error', function (error) {
-    console.error(
-        chalk.red('Error in MongoDb connection: ' + error)
-    );
-    mongoose.disconnect();
-});
+  console.error(chalk.red('Error in MongoDb connection: ' + error))
+  mongoose.disconnect()
+})
 
 db.on('close', function () {
-    console.log(
-        chalk.red('数据库断开，重新连接数据库')
-    );
-    mongoose.connect(global.myAppConfig.url, { server: { auto_reconnect: true } });
-});
+  console.log(chalk.red('数据库断开，重新连接数据库'))
+  mongoose.connect(
+    process.env.DB_HOST || 'mongodb://localhost:27017/mevntest',
+    { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }
+  )
+})
 
-module.exports = db;
+module.exports = db
